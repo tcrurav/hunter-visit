3cc7c4467ea98daff8ccf11a0d11580f55547bc5
tcrurav <tiburcio.cruz@gmail.com>
2019-11-11 14:05:00 +0000

    stars with local storage working

---
+++ src/app/visit/visit.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Visit } from '../models/visit';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-visit',
+  templateUrl: './visit.page.html',
+  styleUrls: ['./visit.page.scss'],
+})
+export class VisitPage implements OnInit {
+
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true
+  };
+
+  visit: Visit;
+
+  constructor(
+    private router: Router,
+    private api: ApiService) { }
+
+  ngOnInit() {
+    this.visit = this.api.getNowHuntingVisit();
+    // this.route.queryParams.subscribe(params => {
+    //   if (params && params.visit) {
+    //     this.visit = JSON.parse(params.visit);
+    //   }
+    //   // console.log(this.visit);
+    // });
+  }
+
+  answerTheQuestion(){
+    this.router.navigateByUrl('/geolocation');
+  }
+
+}
